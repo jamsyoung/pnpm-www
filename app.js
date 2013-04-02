5cfd20b5c8b84ecfe8acbd27af480ec6c45d3aa5
James Young <james.young@turner.com>
2013-04-01 23:04:01 -0400

    progress

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 /*jslint node: true, nomen: true, regexp: true */
 
-var fs = require('fs'),
-    http = require('http'),
-    path = require('path'),
+var http = require('http'),
     dust = require('dustjs-linkedin'),
     routes = require('./routes'),
     express = require('express'),
@@ -24,7 +22,6 @@ app.configure(function () {
     app.use(express.logger('dev'));    // Need to work out details around this
     app.use(express.compress());
     // app.use(express.bodyParser());     // Not using this at this time
-    // app.use(express.methodOverride()); // Not needed, unless we do DELETES and PUTs
 
     app.use(app.router);
     app.use(express['static'](__dirname + '/public'));
@@ -33,14 +30,10 @@ app.configure(function () {
 });
 
 
-// app.get('/', routes.getFrontPage);
-// app.get('/user/:user', routes.getContentByUser);
+/* routes */
+app.get('/', routes.getFrontPage);
+app.get('/user/:user', routes.getContentByUser);
 app.get('/recently-updated', routes.getRecentlyUpdated);
-
-// app.get(/^(\/|\/index\.(html|json|xml|rss|newsml))$/, routes.getHomepage);
-// app.get(/^.+(html|json|xml|rss|newsml)$/, routes.getContentByUrl);
-
-
 
 
 http.createServer(app).listen(app.get('port'), function () {
--- routes/index.js
+++ routes/index.js
@@ -16,28 +16,47 @@ module.exports = {
         });
     },
 
+
+
     getFrontPage: function (req, res) {
         'use strict';
 
-        db.getAllPackages(function (content, error) {
-            if (error) {
-                console.log(error);
-            }
+        var contentModel = {};
 
-            res.render('index', {
-                content: {
-                    totalPackages: content.total_rows
-                }
+        // db.getAllPackages(function (content, error) {
+        //     if (error) {
+        //         console.log(error);
+        //     }
+
+        //     res.render('index', {
+        //         content: {
+        //             totalPackages: content.total_rows
+        //         }
+        //     });
+        // });
+
+        db.getAllPackages(function (content, error) {
+            if (error) { console.log(error); }
+
+            contentModel.totalPackages = content.total_rows;
+
+            db.getRecentlyUpdated({ 'limit': 10 }, function (content, error) {
+                if (error) { console.log(error); }
+                contentModel.recentlyUpdated = content;
+
+                res.render('index', { content: contentModel });
             });
         });
     },
 
+
+
     getRecentlyUpdated: function (req, res) {
         'use strict';
 
-        db.getRecentlyUpdated({ 'limit': 10, showDetails: false }, function (content, error) {
+        db.getRecentlyUpdated({ 'limit': 10, include_docs: true, descending: true }, function (content, error) {
             if (error) {
-                console.log(error);
+                console.log('ERROR: ' + error);
             }
             res.render('index', { content: content });
         });
